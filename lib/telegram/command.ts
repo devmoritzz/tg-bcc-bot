62bf297aff83753db9e5d33a29526354c3338b2d
h11 <nirisix@gmail.com>
2020-02-11 21:15:07 +0800

    trim bot name

--- lib/telegram/command.ts
+++ lib/telegram/command.ts
@@ -2,6 +2,8 @@ import rollbar from '../utils/rollbar'
 import model from '../models'
 import { sendMessage } from './api'
 
+const BOT_NAME_RE = new RegExp(`@${process.env.TG_BOT_NAME}$`, 'i')
+
 const listHashtag = async (chat: number) => {
     const tags = await model.getTags(chat)
     const text = tags.length === 0 ? 'not found' : tags.join('\n')
@@ -12,7 +14,8 @@ const listHashtag = async (chat: number) => {
     })
 }
 
-export const dispatch = (cmd: string, chat: number) => {
+export const dispatch = (command: string, chat: number) => {
+    const cmd = command.replace(BOT_NAME_RE, '')
     if (cmd === '/list_hashtag') {
         listHashtag(chat)
     } else {
